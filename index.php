651e458123c4e8e27740464b8d9f256d2c6035f5
akrilaplekis <49913764+akrilaplekis@users.noreply.github.com>
2022-10-07 21:30:42 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8", name="viewport" content="width=device-width, initial-scale=1">
+        <title>Zvaigznīte</title>
+        <link href="index.css" rel="stylesheet" type="text/css">
+        <link href="icons.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div>
+            <button class="landingBtn homeButton">Zvaigznīte</button>
+            <button class="landingBtn grupasBtn"><span>Grupas</span></button>
+        </div>
+        <div class="textBlock">
+            <div class="line1">
+                <h2 class="landingH2">Par mums</h2>
+            </div>
+            <p class="textBlue">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+        <div class="fotoBlock">
+            
+                <h2 class="landingH2">Foto Galerija</h2>
+            <div class="folderImg">&nbsp;</div>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
